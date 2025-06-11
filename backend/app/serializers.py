8235869301bcbb076ceea576b2d6c9290b45bf48
GabrielBoscoDeolindo <gabibosco01@gmail.com>
2025-06-11 10:31:57 -0300

    clean code no backend

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -2,21 +2,25 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Sensor, Ambiente, Historico
 
+# -------------- Serializer de Sensores ----------------
 class SensorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sensor
         fields = '__all__'
 
+# -------------- Serializer de Ambientes ----------------
 class AmbienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ambiente
         fields = '__all__'
 
+# -------------- Serializer de Historico ----------------
 class HistoricoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Historico
         fields = '__all__'
 
+# --------- Serializer de Registro de Usuário ------------
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
--- backend/app/urls.py
+++ backend/app/urls.py
@@ -3,18 +3,23 @@ from .views import *
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
+    # urls de cadastro e login
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     
+    # urls de sensores
     path('sensores/', SensorListCreateView.as_view(), name='sensores'),
     path('sensores/<int:pk>/', SensorDetailView.as_view(), name='sensor-detail'),
     
+    # urls de ambientes
     path('ambientes/', AmbienteListCreateView.as_view(), name='ambientes'),
     path('ambientes/<int:pk>/', AmbienteDetailView.as_view(), name='ambiente-detail'),
     
+    # urls de historico
     path('historico/', HistoricoListCreateView.as_view(), name='historico'),
     path('historico/<int:pk>/', HistoricoDetailView.as_view(), name='historico-detail'),
 
+    # urls para exportar dados
     path('exportar-sensor', ExportarSensor.as_view(), name='exportar-excel-sensores'),
     path('exportar-ambiente', ExportarAmbiente.as_view(), name='exportar-excel-sensores-ambiente'),
     path('exportar-historico', ExportarHistorico.as_view(), name='exportar-excel-sensores-historico'),
